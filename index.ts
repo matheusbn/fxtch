4e932414c9cd05d2f992d0f838214758fa7c1342
Matheus Berkenbrock Nedel <matheusbnedel@gmail.com>
2021-10-08 00:14:13 -0300

    add list of public apis

--- index.ts
+++ index.ts
@@ -4,6 +4,20 @@ import fetch from 'cross-fetch'
 // /api/characters?category=Better+Call+Saul
 
 // const api = x.create('https://www.breakingbadapi.com/api')
+// Public APIS
+;[
+  'https://api.publicapis.org/entries',
+  'https://catfact.ninja/fact',
+  'https://api.coindesk.com/v1/bpi/currentprice.json',
+  'https://www.boredapi.com/api/activity',
+  'https://datausa.io/api/data?drilldowns=Nation&measures=Population',
+  'https://dog.ceo/api/breeds/image/random',
+  'https://api.ipify.org?format=json',
+  'https://official-joke-api.appspot.com/random_joke',
+  'https://randomuser.me/api/',
+  'http://universities.hipolabs.com/search?country=United+States',
+  'https://api.zippopotam.us/us/33162',
+]
 
 async function main() {
   // const res = await fxtch('https://www.breakingbadapi.com/api/characters')
